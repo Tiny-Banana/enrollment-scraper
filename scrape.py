26bc1e24126e90bed9fdba41f374ed5b7f7e3e9e
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-12-16 20:24:17 +0800

    check page source

--- scrape.py
+++ scrape.py
@@ -18,47 +18,48 @@ class UndetectedTest(BaseCase):
             self.get_new_driver(undetectable=True)
         self.driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/#relax")
 
-        idnum = self.get_element("input[name='p_id_no']")
-        idnum.send_keys("12108084")
-        idnum.send_keys(Keys.RETURN)
+        print(self.driver.page_source)
+        # idnum = self.get_element("input[name='p_id_no']")
+        # idnum.send_keys("12108084")
+        # idnum.send_keys(Keys.RETURN)
         
-        courses = ["STSWENG", "3DMODEL", "CSARCH2", "LBYARCH", "STADVDB", "GAMEDES", "GEARTAP", "GEWORLD"]
-        avail = {}
+        # courses = ["STSWENG", "3DMODEL", "CSARCH2", "LBYARCH", "STADVDB", "GAMEDES", "GEARTAP", "GEWORLD"]
+        # avail = {}
 
-        for course in courses:
-            avail.update({"course": course})
-            print("\n\n" + course + "\n\n")
+        # for course in courses:
+        #     avail.update({"course": course})
+        #     print("\n\n" + course + "\n\n")
             
-            cc = self.get_element("input[name='p_course_code']")
-            cc.send_keys(course)
-            cc.send_keys(Keys.RETURN)
+        #     cc = self.get_element("input[name='p_course_code']")
+        #     cc.send_keys(course)
+        #     cc.send_keys(Keys.RETURN)
 
-            time.sleep(1)
+        #     time.sleep(1)
 
-            page_source = self.driver.page_source
-            soup = BeautifulSoup(page_source, 'html.parser')
+        #     page_source = self.driver.page_source
+        #     soup = BeautifulSoup(page_source, 'html.parser')
 
-            font_elements = soup.find_all("font", {"color": "#006600"})
-            codes = []
+        #     font_elements = soup.find_all("font", {"color": "#006600"})
+        #     codes = []
 
-            # Iterate through each <font> element and get the <b> elements inside them
-            for font_element in font_elements:
-                bold_elements = font_element.find_all("b")
+        #     # Iterate through each <font> element and get the <b> elements inside them
+        #     for font_element in font_elements:
+        #         bold_elements = font_element.find_all("b")
 
-                # Process the bold elements as needed
-                for bold_element in bold_elements:
-                    # Extract information or perform actions on the bold element
-                    text_content = bold_element.text
-                    # Attempt to convert the text content to an integer
-                    try:
-                        number = int(text_content)
-                        # Print only if conversion is successful
-                        codes.append(number)
-                        print(number)
-                    except ValueError:
-                        # Handle the case where conversion to integer fails (e.g., text contains non-numeric characters)
-                        pass
-            avail.update({"codes": codes})
+        #         # Process the bold elements as needed
+        #         for bold_element in bold_elements:
+        #             # Extract information or perform actions on the bold element
+        #             text_content = bold_element.text
+        #             # Attempt to convert the text content to an integer
+        #             try:
+        #                 number = int(text_content)
+        #                 # Print only if conversion is successful
+        #                 codes.append(number)
+        #                 print(number)
+        #             except ValueError:
+        #                 # Handle the case where conversion to integer fails (e.g., text contains non-numeric characters)
+        #                 pass
+        #     avail.update({"codes": codes})
 
 
 
