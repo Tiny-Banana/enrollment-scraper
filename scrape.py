647a63a1753d90610a3ae23776892cfd6f0b0b1a
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-12-16 21:10:48 +0800

    add infinite loop

--- scrape.py
+++ scrape.py
@@ -3,6 +3,9 @@ Some sites use scripts to detect Selenium, and then block you.
 To evade detection, add --uc as a pytest command-line option."""
 from seleniumbase import BaseCase
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 import time
@@ -15,53 +18,54 @@ class UndetectedTest(BaseCase):
 
     def test_browser_is_undetected(self):
         if not (self.undetectable):
-            self.get_new_driver(undetectable=True)
+            self.get_new_driver(undetectable=True, headless=True)
         self.driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/#relax")
         
-        time.sleep(10)
         print(self.driver.page_source)
-        # idnum = self.get_element("input[name='p_id_no']")
-        # idnum.send_keys("12108084")
-        # idnum.send_keys(Keys.RETURN)
+        idnum = self.get_element("input[name='p_id_no']")
+        idnum.send_keys("12108084")
+        idnum.send_keys(Keys.RETURN)
         
-        # courses = ["STSWENG", "3DMODEL", "CSARCH2", "LBYARCH", "STADVDB", "GAMEDES", "GEARTAP", "GEWORLD"]
-        # avail = {}
+        courses = ["STSWENG", "3DMODEL", "CSARCH2", "LBYARCH", "STADVDB", "GAMEDES", "GEARTAP", "GEWORLD"]
 
-        # for course in courses:
-        #     avail.update({"course": course})
-        #     print("\n\n" + course + "\n\n")
-            
-        #     cc = self.get_element("input[name='p_course_code']")
-        #     cc.send_keys(course)
-        #     cc.send_keys(Keys.RETURN)
+        while True:
+            avail = {}
 
-        #     time.sleep(1)
+            for course in courses:
+                avail.update({"course": course})
+                print("\n\n" + course + "\n\n")
+                
+                cc = self.get_element("input[name='p_course_code']")
+                cc.send_keys(course)
+                cc.send_keys(Keys.RETURN)
 
-        #     page_source = self.driver.page_source
-        #     soup = BeautifulSoup(page_source, 'html.parser')
+                time.sleep(0.5)
 
-        #     font_elements = soup.find_all("font", {"color": "#006600"})
-        #     codes = []
+                page_source = self.driver.page_source
+                soup = BeautifulSoup(page_source, 'html.parser')
 
-        #     # Iterate through each <font> element and get the <b> elements inside them
-        #     for font_element in font_elements:
-        #         bold_elements = font_element.find_all("b")
+                font_elements = soup.find_all("font", {"color": "#006600"})
+                codes = []
 
-        #         # Process the bold elements as needed
-        #         for bold_element in bold_elements:
-        #             # Extract information or perform actions on the bold element
-        #             text_content = bold_element.text
-        #             # Attempt to convert the text content to an integer
-        #             try:
-        #                 number = int(text_content)
-        #                 # Print only if conversion is successful
-        #                 codes.append(number)
-        #                 print(number)
-        #             except ValueError:
-        #                 # Handle the case where conversion to integer fails (e.g., text contains non-numeric characters)
-        #                 pass
-        #     avail.update({"codes": codes})
+                # Iterate through each <font> element and get the <b> elements inside them
+                for font_element in font_elements:
+                    bold_elements = font_element.find_all("b")
 
+                    # Process the bold elements as needed
+                    for bold_element in bold_elements:
+                        # Extract information or perform actions on the bold element
+                        text_content = bold_element.text
+                        # Attempt to convert the text content to an integer
+                        try:
+                            number = int(text_content)
+                            # Print only if conversion is successful
+                            codes.append(number)
+                            print(number)
+                        except ValueError:
+                            # Handle the case where conversion to integer fails (e.g., text contains non-numeric characters)
+                            pass
+                avail.update({"codes": codes})
+            time.sleep(10)
 
 
 # def email_alert(subject, body, to):
