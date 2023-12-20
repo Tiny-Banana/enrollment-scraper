c85095c569c3d3f433ace3a958d749b376b827a8
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2023-12-20 21:28:08 +0800

    temporarily fix some errors

--- scrape.py
+++ scrape.py
@@ -11,69 +11,75 @@ def send_telegram_message(message):
     print(requests.get(url).json())
 
 def scraper():
-    driver = Driver(uc=True, headless=True)
-    driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/")
-    print("UA= ", driver.get_user_agent())
-    
-    bypassed = False
-    
-    while not bypassed:
-        try:
-            driver.send_keys("input[name='p_id_no']", "12108084")
-            driver.send_keys("input[name='p_button']", Keys.RETURN)
-            bypassed = True
-        except Exception:
-            print("Failed")
-            driver.delete_all_cookies()
-            driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/")
-
-    courses = ["STSWENG", "3DMODEL", "CSARCH2", "LBYARCH", "STADVDB", "GAMEDES", "HCI2001", "GEWORLD", "MOBDEVE"]
-    preferred = [5789, 6212, 6213, 4307, 4306]
-    dicts = [{"cc": 5789, "ct": "GAMEDES", "sched": "MH 1100 - 1230", "prof": "Ryan Dimaunahan"},
-            {"cc": 6212, "ct": "HCI2001", "sched": "MH 0730 - 0900", "prof": "Jordan"},
-            {"cc": 6213, "ct": "HCI2001", "sched": "M 0730 - 0900 H 0915 - 1045", "prof": "Jordan"},
-            {"cc": 4307, "ct": "CSARCH2", "sched": "MH 1430 - 1600", "prof": "Roger"},
-            {"cc": 4306, "ct": "CSARCH2", "sched": "TF 1100 - 1200", "prof": "Roger"}]
-
     while True:
-        codes = []
-        for course in courses:
-            driver.send_keys("input[name='p_course_code']", course)
-            driver.send_keys("input[name='p_button']", Keys.RETURN)
+        driver = Driver(uc=True, headless=True)
+        driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/")
+        print("UA= ", driver.get_user_agent())
+    
+        bypassed = False
+        
+        while not bypassed:
+            try:
+                driver.send_keys("input[name='p_id_no']", "12108084")
+                driver.send_keys("input[name='p_button']", Keys.RETURN)
+                bypassed = True
+            except Exception:
+                print("Failed")
+                driver.delete_all_cookies()
+                driver.get("https://enroll.dlsu.edu.ph/dlsu/view_course_offerings/")
+        print("You're in")
 
-            time.sleep(0.5)
+        ## wala pa: arch2 lbyarch f
+        courses = ["STADVDB", "GAMEDES", "CSARCH2", "LBYARCH"]
+        preferred = [5789, 4411, 2383,  4306, 4307]
+        dicts = [{"cc": 5789, "ct": "GAMEDES", "sched": "MH 1100 - 1230", "prof": "Ryan"},
+                {"cc": 4411, "ct": "STADVDB", "sched": "TF 1245 - 1415", "prof": "Kerwin"},
+                {"cc": 2383, "ct": "LBYARCH", "sched": "MH 1430 - 1600 ", "prof": "Laguna"},
+                {"cc": 4306, "ct": "CSARCH2", "sched": "TF 1100 - 1230", "prof": "Roger"},
+                {"cc": 4307, "ct": "CSARCH2", "sched": "MH 1430 - 1600", "prof": "Roger"}]
 
-            page_source = driver.page_source
-            soup = BeautifulSoup(page_source, 'html.parser')
+        try:
+            print("Started scraping")
+            while True:
+                codes = []
 
-            font_elements = soup.find_all("font", {"color": "#006600"})
+                for course in courses:
+                    driver.send_keys("input[name='p_course_code']", course)
+                    driver.send_keys("input[name='p_button']", Keys.RETURN)
 
-            for font_element in font_elements:
-                bold_elements = font_element.find_all("b")
+                    page_source = driver.page_source
+                    soup = BeautifulSoup(page_source, 'html.parser')
 
-                for bold_element in bold_elements:
-                    text_content = bold_element.text
-                    try:
-                        number = int(text_content)
+                    font_elements = soup.find_all("font", {"color": "#006600"})
 
-                        if number in preferred:
-                            codes.append(number)
+                    for font_element in font_elements:
+                        bold_elements = font_element.find_all("b")
 
-                    except ValueError:
-                        pass
+                        for bold_element in bold_elements:
+                            text_content = bold_element.text
+                            try:
+                                number = int(text_content)
 
-        message = ""
+                                if number in preferred:
+                                    codes.append(number)
 
-        for code in codes:
-            for d in dicts:
-                if d["cc"] == code:
-                    message += f"Code: {code}\nCourse: {d['ct']}\nSchedule: {d['sched']}\nProf: {d['prof']}\n\n"
-                    print(message)
+                            except ValueError:
+                                pass
 
-        if message != "":
-            send_telegram_message(message)
+                message = ""
 
-        time.sleep(10)
+                for code in codes:
+                    for d in dicts:
+                        if d["cc"] == code:
+                            message += f"Code: {code}\nCourse: {d['ct']}\nSchedule: {d['sched']}\nProf: {d['prof']}\n\n"
+                            print(message)
+
+                if message != "":
+                    send_telegram_message(message)
+
+                time.sleep(15)
+        except Exception: 
+            print("Error encountered. Trying to restart")
 
 if __name__ == "__main__":
     scraper()
